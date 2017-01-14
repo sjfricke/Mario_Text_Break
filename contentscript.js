aa5f84513b21fd031f4d8199492401286b8f5da1
sjfricke <sjfricke@wisc.edu>
2017-01-14 16:58:00 -0600

    Some work
    Probably going to call it due to the lack of Three.js support with a
    chrome extension

--- contentscript.js
+++ contentscript.js
@@ -1,7 +1,35 @@
-var s = document.createElement('script');
+// NOTE: Order of script is order of loading
+var files = ["lib/three.js", "script.js", "script2.js"];
+
+addScriptFile(files[0], function(){
+    addScriptFile(files[1], function(){
+        addScriptFile(files[2], function(){
+//            addScriptFile(files[3], function(){
+//        
+//            })
+        })
+    })
+})
+
+
+function addScriptFile(fileURL, callback) {
+    var s = document.createElement('script');
+    s.src = chrome.extension.getURL(fileURL);
+    (document.head||document.documentElement).appendChild(s);
+    s.onload = function() {
+        s.parentNode.removeChild(s);
+//        callback("Done with " + fileURL);
+        console.log("Done with " + fileURL);
+        callback();
+    };
+}
+
+
+/*var s = document.createElement('script');
 s.src = chrome.extension.getURL('script.js');
 (document.head||document.documentElement).appendChild(s);
 s.onload = function() {
+    console.log("script");
     s.parentNode.removeChild(s);
 };
 
@@ -9,6 +37,7 @@ var s2 = document.createElement('script');
 s2.src = chrome.extension.getURL('js/three.min.js');
 (document.head||document.documentElement).appendChild(s2);
 s2.onload = function() {
+    console.log("Three");
     s2.parentNode.removeChild(s2);
 };
 
@@ -16,5 +45,6 @@ var s3 = document.createElement('script');
 s3.src = chrome.extension.getURL('script2.js');
 (document.head||document.documentElement).appendChild(s3);
 s3.onload = function() {
+    console.log("script2");
     s3.parentNode.removeChild(s3);
-};
+};*/
--- script2.js
+++ script2.js
@@ -1,17 +1,42 @@
-setTimeout(function(){ 
+// set timeout to allow other libraries to load
+// TODO fix this NOW 
+//setTimeout(function(){ 
 var camera, scene, renderer;
 var mesh;
 init();
 animate();
 
+
 function init() {
     camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 1, 1000 );
-    camera.position.z = 400;
+    camera.position.z =200;
     scene = new THREE.Scene();
+    
+//    var mario_path = chrome.extension.getURL("models/test_mario/");
+    var mario_model = chrome.extension.getURL("models/test_mario/mario_v01.json");
+//    
+ 
+    
+    var loader = new THREE.JSONLoader();
+    loader.load( mario_model, function( geometry_mario ) {
+        mesh_mario = new THREE.Mesh( 
+            geometry_mario,
+            new THREE.MeshBasicMaterial( { color: new THREE.Color( 0xff0000 ) } )
+        );
+        mesh_mario.visible = true;
+        mesh_mario.scale.set(100,100,100);
+        scene.add( mesh_mario );
+        console.log(mesh_mario.position);
+    } );   
+//    var parsed_mario = loader.parse(mario_model);
+//    var model = new THREE.Mesh( parsed_mario.geometry, new THREE.MeshBasicMaterial() );
+//    scene.add( model );    
+
+    
     var geometry = new THREE.BoxBufferGeometry( 200, 200, 200 );
     var material = new THREE.MeshBasicMaterial( { color: new THREE.Color( 0xff0000 ) } );
     mesh = new THREE.Mesh( geometry, material );
-    scene.add( mesh );
+    //scene.add( mesh );
    renderer = new THREE.WebGLRenderer({
       antialias: true,
       alpha: true
@@ -22,6 +47,13 @@ function init() {
     document.getElementById("mainCanvas").appendChild( renderer.domElement );
     //
     window.addEventListener( 'resize', onWindowResize, false );
+    
+    particleLight = new THREE.Mesh( 
+        new THREE.SphereGeometry( 4, 8, 8 ), 
+        new THREE.MeshBasicMaterial( { color: 0xffffff } ) 
+    );
+    scene.add( particleLight );
+    
 }
 function onWindowResize() {
     camera.aspect = 1;
@@ -33,4 +65,13 @@ function animate() {
     mesh.rotation.x += 0.005;
     mesh.rotation.y += 0.01;
     renderer.render( scene, camera );
-} }, 3000);
+} 
+
+// After loading JSON from our file, we add it to the scene
+function addModelToScene( geometry, materials ) {
+    var material = new THREE.MultiMaterial(materials);
+    model = new THREE.Mesh( geometry, material );
+    model.scale.set(0.5,0.5,0.5);
+    scene.add( model );
+}
+//}, 3000);
